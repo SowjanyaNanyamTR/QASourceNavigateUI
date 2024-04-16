342bfe6267b3f63e7835adc467a9163214eefc96
C288876 <Bhuvaneswari.golthi@thomsonreuters.com>
2024-04-16 14:12:10 +0530

    Updated TCs for PREP Tracking and Proposed/Approved Tabs

--- src/com/thomsonreuters/codes/codesbench/quality/pageelements/sourcenavigateangular/SourceNavigateAngularSectionPageElements.java
+++ src/com/thomsonreuters/codes/codesbench/quality/pageelements/sourcenavigateangular/SourceNavigateAngularSectionPageElements.java
@@ -6,23 +6,29 @@ import org.openqa.selenium.support.How;
 
 import static com.thomsonreuters.codes.codesbench.quality.pageelements.sourcenavigateangular.SourceNavigateAngularPageElements.*;
 
-public class SourceNavigateAngularSectionPageElements
-{
+public class SourceNavigateAngularSectionPageElements {
     public static final String SOURCE_NAV_SECTIONS_AG_GRID = "";
     public static final String FIRST_RENDITION_ROW = "(" + SOURCE_NAV_SECTIONS_AG_GRID + "//div[@row-id='0'])[2]";
     public static final String TOTAL_SECTIONS_NUMBERS = "//span[contains(text(),'Total sections:')]";
     public static final String TOTAL_SECTIONS = "//span[contains(text(),'Total sections: %s')]";
-    public static final String TOTAL_SECETION_NUMBERS="//span[contains(text(),'Total sections: ')]";
-    public static final String CLEAR_FILTERS_SECTION_TAB =  SOURCE_NAV_SECTION + CLEAR_FILTERS;
+    public static final String TOTAL_SECETION_NUMBERS = "//span[contains(text(),'Total sections: ')]";
+    public static final String CLEAR_FILTERS_SECTION_TAB = SOURCE_NAV_SECTION + CLEAR_FILTERS;
     public static final String EDIT_SECTION_INSTRUCTION_NOTES = "Edit Section Instruction Notes";
-    public static final String SECTION_LOCKED_BY ="//p[contains(text(),' This Section is currently locked by ')]";
+    public static final String SECTION_LOCKED_BY = "//p[contains(text(),' This Section is currently locked by ')]";
     public static final String VIEW_SECTION_INSTRUCTION_NOTES = "View Section Instruction Notes";
     public static final String NOTES_VALUE_OF_SECTION = "(//button[@class='grid-btn'])[2]";
     public static final String SECTION_PROPERTIES_BOX_LABEL = "//label[text()='%s']";
-    public static final String SECTION_PROPERTIES_INPUT_FIELD = SECTION_PROPERTIES_BOX_LABEL+"/input";
-    public static final String SYSTEM_PROPERTIES_INPUT_FIELD = "//source-nav-section-properties"+SECTION_PROPERTIES_INPUT_FIELD;
-    public static final String SECTION_PROPERTIES_TEXT_FIELD = SECTION_PROPERTIES_BOX_LABEL+"/following::input[1]";
-    public static final String CALENDAR_OPTION = SECTION_PROPERTIES_BOX_LABEL+"/following::button[@aria-label='Open calendar'][1]";
+    public static final String SECTION_PROPERTIES_INPUT_FIELD = SECTION_PROPERTIES_BOX_LABEL + "/input";
+    public static final String SYSTEM_PROPERTIES_INPUT_FIELD = "//source-nav-section-properties" + SECTION_PROPERTIES_INPUT_FIELD;
+    public static final String SECTION_PROPERTIES_TEXT_FIELD = SECTION_PROPERTIES_BOX_LABEL + "/following::input[1]";
+    public static final String CALENDAR_OPTION = SECTION_PROPERTIES_BOX_LABEL + "/following::button[@aria-label='Open calendar'][1]";
+    public static final String LABEL_TEXT_FIELD = SECTION_PROPERTIES_BOX_LABEL + "/following::input[1]";
+    public static final String OWNER_DATA = SECTION_PROPERTIES_BOX_LABEL + "/following-sibling::input[1]";
+    public static final String SECTION_INSTRUCTIONS = SECTION_PROPERTIES_BOX_LABEL+"/textarea";
+    public static final String PREP_TRACKING_FIRST_HALF = "//div[@class='properties-container mb-3']/div[1]"+SECTION_PROPERTIES_BOX_LABEL;
+    public static final String PREP_TRACKING_SECOND_HALF = "//div[@class='properties-container mb-3']/div[2]"+SECTION_PROPERTIES_BOX_LABEL;
+    public static final String SECTION_PROPERTIES_DIFFICULTY_LEVEL_DROPDOWN="//source-nav-single-select-combobox[@id='difficultyLevels']";
+    public static final String DIFFICULTY_LEVEL_DROPDOWN_VALUE="//div[text()='%s']";
 
     @FindBy(how = How.ID, using = "sect-note-text-area")
     public static WebElement sectionLevelinstructions;
--- src/com/thomsonreuters/codes/codesbench/quality/tests/sourcenavigateangular/general/SNGeneralSectionPropertiesPageTests.java
+++ src/com/thomsonreuters/codes/codesbench/quality/tests/sourcenavigateangular/general/SNGeneralSectionPropertiesPageTests.java
@@ -19,6 +19,8 @@ import static com.thomsonreuters.codes.codesbench.quality.pageelements.sourcenav
 import static com.thomsonreuters.codes.codesbench.quality.pageelements.sourcenavigateangular.SourceNavigateAngularContextMenuItemsPageElements.SUBMIT;
 import static com.thomsonreuters.codes.codesbench.quality.pageelements.sourcenavigateangular.SourceNavigateAngularLeftSidePaneElements.FIND_TEXT_FIELD_PATTERN;
 import static com.thomsonreuters.codes.codesbench.quality.pageelements.sourcenavigateangular.SourceNavigateAngularPageElements.SECTION_ROW;
+import static com.thomsonreuters.codes.codesbench.quality.pageelements.sourcenavigateangular.SourceNavigateAngularSectionPageElements.*;
+import static com.thomsonreuters.codes.codesbench.quality.pageelements.sourcenavigateangular.SourceNavigateAngularSectionPageElements.DIFFICULTY_LEVEL_DROPDOWN;
 import static com.thomsonreuters.codes.codesbench.quality.pageelements.sourcenavigateangular.SourceNavigateAngularTabsPageElements.*;
 import static com.thomsonreuters.codes.codesbench.quality.pageelements.sourcenavigateangular.popups.SourceNavigateAngularPopUpPageElements.*;
 import static com.thomsonreuters.codes.codesbench.quality.utilities.database.BaseDatabaseUtils.disconnect;
@@ -55,11 +57,15 @@ public class SNGeneralSectionPropertiesPageTests extends SourceNavigateAngularAs
         disconnect(connection);
     }
 
+    /**
+     * Test Case ID:723101_TC1
+     * Description:Section Properties: verify the box user interface
+     */
     @Test
     @CustomAnnotations.BrowserAnnotations.EDGE
     @CustomAnnotations.UserAnnotations.LEGAL
     @CustomAnnotations.LogAnnotations.LOG
-    public void verifySectionPropertiesUserInterface() {
+    public void verifySectionPropertiesBoxUserInterface() {
         //Storing APV and PREP Rendition UUIDs
         String[] renditionUUIDs = {renditionAPVUuid, renditionPREPUuid};
 
@@ -103,6 +109,10 @@ public class SNGeneralSectionPropertiesPageTests extends SourceNavigateAngularAs
         }
     }
 
+    /**
+     * Test Case ID:723101_TC2
+     * Description:Section Properties: Section properties tab: verify the tab UI content and functionality
+     */
     @Test
     @CustomAnnotations.BrowserAnnotations.EDGE
     @CustomAnnotations.UserAnnotations.LEGAL
@@ -130,19 +140,51 @@ public class SNGeneralSectionPropertiesPageTests extends SourceNavigateAngularAs
             //Verifying fields in Section Properties Tab
             assertThatFieldsViewMode("Section Number ", "Section Delta Count ");
             assertThatTextFieldsAndCalendarOptions("Effective Date", "Assigned Date");
+            assertThatDisplayOfDropdowns("Assigned User", "Difficulty Level");
+            assertThatDisplayOfTextArea("Section Instructions ");
 
-            sourceNavigateAngularPage().clickCancel();
+            String currentDate = DateAndTimeUtils.getCurrentDateMMddyyyyNoDelimeters();
+            sourceNavigateAngularPage().click(format(CALENDAR_OPTION, "Effective Date"));
+            sourceNavigateAngularPage().sendTextToTextbox(format(LABEL_TEXT_FIELD, "Effective Date"), currentDate);
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+
+            sourceNavigateAngularPage().click(format(CALENDAR_OPTION, "Assigned Date"));
+            sourceNavigateAngularPage().sendTextToTextbox(format(LABEL_TEXT_FIELD, "Assigned Date"), currentDate);
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+
+            sourceNavigateAngularPage().click(format(LABEL_TEXT_FIELD, "Assigned User"));
+            sourceNavigateAngularDeltaPage().sendKeys("TLE TCBA-BOT");
+            sourceNavigateAngularLockReportPage().pressEnter();
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+
+            sourceNavigateAngularDeltaPage().click(SECTION_PROPERTIES_DIFFICULTY_LEVEL_DROPDOWN);
+            sourceNavigateAngularDeltaPage().click(String.format(DIFFICULTY_LEVEL_DROPDOWN_VALUE, "E1"));
+            sourceNavigateAngularPage().waitForPageLoaded();
+
+//            sourceNavigateAngularPage().click(format(LABEL_TEXT_FIELD, "Difficulty Level"));
+//            sourceNavigateAngularDeltaPage().sendKeys("E1");
+//            sourceNavigateAngularLockReportPage().pressEnter();
+            sourceNavigateAngularPage().sendTextToTextbox(format(SECTION_INSTRUCTIONS, "Section Instructions "), "This is testing data for Section Instructions");
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.FOUR_SECONDS);
+
+            sourceNavigateAngularPage().click(SUBMIT);
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+
+            sourceNavigateAngularPage().rightClick((format(SECTION_ROW, 0)));
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+            sourceNavigateAngularPage().clickContextMenuItem(SECTION_PROPERTIES);
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
 
             //Closing Section Properties Pop Up
+            sourceNavigateAngularPage().clickCancel();
             DateAndTimeUtils.takeNap(DateAndTimeUtils.ONE_SECOND);
             assertThatDisplayOfPopUpElements("Confirmation", "Are you sure you want to cancel?", SOURCE_NAV_ASSIGN_USER_CANCEL, CONFIRM_BUTTON, CLOSE_UI_BUTTON);
 
             sourceNavigateAngularPage().click(SOURCE_NAV_ASSIGN_USER_CANCEL);
             assertThatPopUpMessageDisappearedOrNot("Are you sure you want to cancel?");
-//            assertThatDisplayOfPopUpCloseUIButton("Confirmation");
+
             sourceNavigateAngularPage().click(CANCEL_BUTTON);
             assertThatDisplayOfPopUpElements("Confirmation", "Are you sure you want to cancel?", CANCEL_BUTTON, CONFIRM_BUTTON, CLOSE_UI_BUTTON);
-
             sourceNavigateAngularPage().click(CONFIRM_BUTTON);
 
             DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
@@ -150,8 +192,10 @@ public class SNGeneralSectionPropertiesPageTests extends SourceNavigateAngularAs
             DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
             sourceNavigateAngularPage().clickContextMenuItem(SECTION_PROPERTIES);
             DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+
             sourceNavigateAngularPage().click(CANCEL_BUTTON);
             assertThatDisplayOfPopUpElements("Confirmation", "Are you sure you want to cancel?", CANCEL_BUTTON, CONFIRM_BUTTON, CLOSE_UI_BUTTON);
+
             sourceNavigateAngularPage().click(format(CLOSE_UI_BUTTON, "Confirmation"));
             assertThatPopUpMessageDisappearedOrNot("Are you sure you want to cancel?");
 
@@ -162,11 +206,13 @@ public class SNGeneralSectionPropertiesPageTests extends SourceNavigateAngularAs
 
             sourceNavigateAngularPage().click(format(CLOSE_UI_BUTTON, headerText));
             assertThatDisplayOfPopUpElements("Confirmation", "Are you sure you want to cancel?", CANCEL_BUTTON, CONFIRM_BUTTON, CLOSE_UI_BUTTON);
+
             sourceNavigateAngularPage().click(format(CLOSE_UI_BUTTON, "Confirmation"));
             assertThatPopUpMessageDisappearedOrNot("Are you sure you want to cancel?");
 
             sourceNavigateAngularPage().click(format(CLOSE_UI_BUTTON, headerText));
             assertThatDisplayOfPopUpElements("Confirmation", "Are you sure you want to cancel?", CANCEL_BUTTON, CONFIRM_BUTTON, CLOSE_UI_BUTTON);
+
             sourceNavigateAngularPage().click(CONFIRM_BUTTON);
             assertThatPopUpMessageDisappearedOrNot("Are you sure you want to cancel?");
 
@@ -178,6 +224,133 @@ public class SNGeneralSectionPropertiesPageTests extends SourceNavigateAngularAs
         }
     }
 
+    /**
+     * Test Case ID:723101_TC4
+     * Description:Section Properties: Proposed/Approved Tracking Information tab: verify the tab UI content and functionality
+     */
+    @Test
+    @CustomAnnotations.BrowserAnnotations.EDGE
+    @CustomAnnotations.UserAnnotations.LEGAL
+    @CustomAnnotations.LogAnnotations.LOG
+    public void verifyProposedApprovedTrackingInformationContentAndFunctionality() {
+        //Storing APV and PREP Rendition UUIDs
+        String[] renditionUUIDs = {renditionAPVUuid};
+        String loggedUserName = "TLE TCBA-BOT";
+
+        String[] attributeLabelNames = {"Images Sent Out", "Images Completed", "Tabular Requested", "Tabular Started", "Tabular Completed"};
+        String currentDate = DateAndTimeUtils.getCurrentDateMMddyyyyNoDelimeters();
+
+        System.out.println("Current Date:" + currentDate + "/");
+        //Finding the Renditions with Rendition UUID with APV and PREP
+        sourceNavigateAngularLeftSidePanePage().clickFindButtonOnLeftPane();
+        for (String uuid : renditionUUIDs) {
+            sourceNavigateAngularLeftSidePanePage().setFindValue("Rendition UUID", uuid);
+            sourceNavigateAngularLeftSidePanePage().clickFindButton();
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.THREE_SECONDS);
+            sourceNavigateAngularPage().rightClickRenditions();
+
+            //Section tab clicking and selecting first row
+            sourceNavigateAngularTabsPage().click(SECTION_TAB);
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+            sourceNavigateAngularPage().rightClick((format(SECTION_ROW, 0)));
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+            sourceNavigateAngularPage().clickContextMenuItem(SECTION_PROPERTIES);
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+
+            sourceNavigateAngularPage().click(format(ANY_TAB_NAME, "Proposed/Approved Tracking Information"));
+
+            assertThatProposedApprovedTrackingInformationTabContainsColumns("Attribute", "Date", "Owner");
+
+            for (String labelName : attributeLabelNames)
+                assertThatTabTextFieldAndCalendarOption(labelName);
+            for (String labelName : attributeLabelNames) {
+                sourceNavigateAngularDeltaPage().click(format(CALENDAR_OPTION, labelName));
+                sourceNavigateAngularPage().sendTextToTextbox(format(LABEL_TEXT_FIELD, labelName), currentDate);
+            }
+
+            //Closing Section Properties Pop Up
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.ONE_SECOND);
+            sourceNavigateAngularPage().click(SUBMIT);
+
+            sourceNavigateAngularPage().rightClick((format(SECTION_ROW, 0)));
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+            sourceNavigateAngularPage().clickContextMenuItem(SECTION_PROPERTIES);
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+            sourceNavigateAngularPage().click(format(ANY_TAB_NAME, "Proposed/Approved Tracking Information"));
+
+            for (String labelName : attributeLabelNames) {
+                String currentDateWithDelimeter = DateAndTimeUtils.getCurrentDateMMddyyyy();
+                assertThatCurrentDateSaved(labelName, currentDateWithDelimeter);
+                assertThatColumnPopulatedWithOwnerData(labelName, loggedUserName);
+            }
+
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.ONE_SECOND);
+            sourceNavigateAngularPage().clickCancel();
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+            assertThatDisplayOfPopUpElements("Confirmation", "Are you sure you want to cancel?", CANCEL_BUTTON, CONFIRM_BUTTON, CLOSE_UI_BUTTON);
+            sourceNavigateAngularPage().click(SOURCE_NAV_ASSIGN_USER_CANCEL);
+            //Closing Section Properties Pop Up
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.ONE_SECOND);
+            sourceNavigateAngularPage().clickCancel();
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.ONE_SECOND);
+            sourceNavigateAngularPage().clickConfirm();
+
+            //Navigating to Rendition Tab
+            sourceNavigateAngularTabsPage().click(RENDITION_TAB);
+            sourceNavigateAngularLeftSidePanePage().clear(String.format(FIND_TEXT_FIELD_PATTERN, "Rendition UUID"));
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+
+        }
+    }
+
+    /**
+     * Test Case ID:723101_TC5
+     * Description:Section Properties: PREP Tracking Information: verify the tab UI content and functionality
+     */
+    @Test
+    @CustomAnnotations.BrowserAnnotations.EDGE
+    @CustomAnnotations.UserAnnotations.LEGAL
+    @CustomAnnotations.LogAnnotations.LOG
+    public void verifyPREPTrackingInformationTabContentAndFunctionality() {
+        String[] renditionUUIDs = {renditionPREPUuid};
+
+        sourceNavigateAngularLeftSidePanePage().clickFindButtonOnLeftPane();
+        for (String uuid : renditionUUIDs) {
+            sourceNavigateAngularLeftSidePanePage().setFindValue("Rendition UUID", uuid);
+            sourceNavigateAngularLeftSidePanePage().clickFindButton();
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.THREE_SECONDS);
+            sourceNavigateAngularPage().rightClickRenditions();
+
+            //Section tab clicking and selecting first row
+            sourceNavigateAngularTabsPage().click(SECTION_TAB);
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+            sourceNavigateAngularPage().rightClick((format(SECTION_ROW, 0)));
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+            sourceNavigateAngularPage().clickContextMenuItem(SECTION_PROPERTIES);
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+
+            sourceNavigateAngularPage().click(format(ANY_TAB_NAME, "PREP Tracking Information"));
+            assertThatPREPTrackingFirstHalfColumns("Attribute","Value","Owner");
+            assertThatPREPTrackingSecondHalfColumns("Attribute","Value","Owner");
+
+            //Closing Section Properties Pop Up
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.ONE_SECOND);
+            sourceNavigateAngularPage().clickCancel();
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.ONE_SECOND);
+            sourceNavigateAngularPage().clickConfirm();
+
+            //Navigating to Rendition Tab
+            sourceNavigateAngularTabsPage().click(RENDITION_TAB);
+            sourceNavigateAngularLeftSidePanePage().clear(String.format(FIND_TEXT_FIELD_PATTERN, "Rendition UUID"));
+            DateAndTimeUtils.takeNap(DateAndTimeUtils.TWO_SECONDS);
+
+        }
+    }
+
+    /**
+     * Test Case ID:723101_TC6
+     * Description:Section Properties: System Properties: verify the tab UI content and functionality
+     */
     @Test
     @CustomAnnotations.BrowserAnnotations.EDGE
     @CustomAnnotations.UserAnnotations.LEGAL
